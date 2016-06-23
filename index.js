7398fbafa68dbe0fe6f3b2661649654e637258d7
Ralph Broers <ralph.broers@luminis.eu>
2016-06-23 20:25:10 +0200

    Stripping everything but the leaflet sample

--- index.js
+++ index.js
@@ -1,30 +1,24 @@
-import exampleRoute from './server/routes/example';
-
 export default function (kibana) {
-  return new kibana.Plugin({
-    require: ['elasticsearch'],
+    return new kibana.Plugin({
+        require: ['elasticsearch'],
 
-    uiExports: {
-      app: {
-        title: 'Kibana Leaflet Sample',
-        description: 'Sample kibana leaflet plugin',
-        main: 'plugins/kibana_leaflet_sample/app'
-      },
-      hacks: [
-        'plugins/kibana_leaflet_sample/hack'
-      ]
-    },
+        uiExports: {
+            app: {
+                title: 'Kibana Leaflet Sample',
+                description: 'Sample kibana leaflet plugin',
+                main: 'plugins/kibana_leaflet_sample/app'
+            }
+        },
 
-    config(Joi) {
-      return Joi.object({
-        enabled: Joi.boolean().default(true),
-      }).default();
-    },
+        config(Joi) {
+            return Joi.object({
+                enabled: Joi.boolean().default(true),
+            }).default();
+        },
 
-    init(server, options) {
-      // Add server routes and initalize the plugin here
-      exampleRoute(server);
-    }
+        init(server, options) {
+            // Add server routes and initalize the plugin here
+        }
 
-  });
+    });
 };
--- public/app.js
+++ public/app.js
@@ -1,5 +1,3 @@
-import moment from 'moment';
-import chrome from 'ui/chrome';
 import uiModules from 'ui/modules';
 import uiRoutes from 'ui/routes';
 
@@ -7,33 +5,30 @@ import 'ui/autoload/styles';
 import './less/main.less';
 import template from './templates/index.html';
 
-chrome
-  .setNavBackground('#222222')
-  .setTabs([]);
+import 'leaflet/dist/leaflet.css';
+import 'leaflet';
+import 'angular-simple-logger/dist/index.light';
+import 'ui-leaflet';
 
 uiRoutes.enable();
 uiRoutes
-.when('/', {
-  template,
-  resolve: {
-    currentTime($http) {
-      return $http.get('../api/kibana_leaflet_sample/example').then(function (resp) {
-        return resp.data.time;
-      });
-    }
-  }
-});
+    .when('/', {
+        template
+    });
 
 uiModules
-.get('app/kibana_leaflet_sample', [])
-.controller('kibanaLeafletSampleHelloWorld', function ($scope, $route, $interval) {
-  $scope.title = 'Kibana Leaflet Sample';
-  $scope.description = 'Sample kibana leaflet plugin';
+    .get('app/kibana_leaflet_sample', ['ui-leaflet'])
+    .controller('kibanaLeafletSampleHelloWorld', function ($scope) {
+        $scope.title = 'Kibana Leaflet Sample';
+        $scope.description = 'Sample kibana leaflet plugin';
 
-  var currentTime = moment($route.current.locals.currentTime);
-  $scope.currentTime = currentTime.format('HH:mm:ss');
-  var unsubscribe = $interval(function () {
-    $scope.currentTime = currentTime.add(1, 'second').format('HH:mm:ss');
-  }, 1000);
-  $scope.$watch('$destroy', unsubscribe);
-});
+        $scope.defaults = {
+            tileLayer: 'http://{s}.tile.opencyclemap.org/cycle/{z}/{x}/{y}.png',
+            maxZoom: 14,
+            path: {
+                weight: 10,
+                color: '#800000',
+                opacity: 1
+            }
+        };
+    });
